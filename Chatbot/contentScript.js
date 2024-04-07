abbf29c1758ba526852a436bf74aa5d78805b1bd
Charan Battula <charanb2005@gmail.com>
2024-04-07 16:35:44 -0700

    Complete

--- Chatbot/contentScript.js
+++ Chatbot/contentScript.js
@@ -1,4 +1,4 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => { 
     if (request.action === "searchForSrc") {
         console.log("BRUH");
 
